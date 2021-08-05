03d52e92ff9882bec184204e9faeb706a6d69e18
Sylvain Desvé <sylvain.desve@gmail.com>
2021-08-05 22:46:34 +0200

    Rewrote Writer factory

--- examples/writer.ts
+++ examples/writer.ts
@@ -11,18 +11,18 @@ export const run = (): void => {
   const john: Person = { name: "John", belongings: ["keys", "shoes"] };
 
   function logEnter(p: Person): Writer<string, Person> {
-    return writer({ value: p, written: [`${p.name} entered the vault`] });
+    return writer([`${p.name} entered the vault`], p);
   }
 
   console.log(logEnter(john));
 
-  const logGetBelongings = writer({
-    value: (person: Person) => person.belongings,
-    written: ["Getting belongings"],
-  });
+  const logGetBelongings = writer(
+    ["Getting belongings"],
+    (person: Person) => person.belongings
+  );
 
   function logValue<T>(v: T): Writer<string, T> {
-    return writer({ value: v, written: [`${JSON.stringify(v)}`] });
+    return writer([`${JSON.stringify(v)}`], v);
   }
 
   console.log(
@@ -42,17 +42,22 @@ export const run = (): void => {
   */
 
   function logNumber(n: number): Writer<string, number> {
-    return writer({ value: n, written: [`got number ${n}`] });
+    return writer([`got number ${n}`], n);
   }
 
   // Desugaring do notation : https://en.wikibooks.org/wiki/Haskell/do_notation
   const multWithLog: Writer<string, number> = logNumber(3).chain((a: number) =>
     logNumber(5).chain((b: number) =>
-      tell(`multiply ${a} with ${b}`).then(
-        writer({ value: a * b, written: [] as string[] })
-      )
+      tell(`multiply ${a} with ${b}`).then(writer([] as string[], a * b))
     )
   );
 
   console.log(multWithLog);
+
+  console.log(
+    writer(["Starting"], 0)
+      .chain((a: number) => writer([`adding 3 to ${a}`], a + 3))
+      .chain((a: number) => writer([`adding 10 to ${a}`], a + 10))
+      .chain((a: number) => writer([`result is ${a}`], a))
+  );
 };
--- src/Writer.ts
+++ src/Writer.ts
@@ -9,7 +9,7 @@ export class Writer<W, T> implements Monad<T> {
   constructor(public readonly runWriter: RunWriter<W, T>) {}
 
   public map<U>(fn: (t: T) => U): Writer<W, U> {
-    return writer({
+    return new Writer({
       value: fn(this.runWriter.value),
       written: this.runWriter.written,
     });
@@ -17,7 +17,7 @@ export class Writer<W, T> implements Monad<T> {
 
   public ap<U>(fn: Writer<W, (t: T) => U>): Writer<W, U> {
     const { value: fnv, written: fnw } = fn.runWriter;
-    return writer({
+    return new Writer({
       value: fnv(this.runWriter.value),
       written: this.runWriter.written.concat(fnw),
     });
@@ -26,7 +26,7 @@ export class Writer<W, T> implements Monad<T> {
   public chain<U>(fn: (t: T) => Writer<W, U>): Writer<W, U> {
     const { value: thisValue, written: thisWritten } = this.runWriter;
     const { value: fnValue, written: fnWritten } = fn(thisValue).runWriter;
-    return writer({
+    return new Writer({
       value: fnValue,
       written: thisWritten.concat(fnWritten),
     });
@@ -40,10 +40,10 @@ export class Writer<W, T> implements Monad<T> {
   }
 }
 
-export function writer<W, T>(runWriter: RunWriter<W, T>): Writer<W, T> {
-  return new Writer(runWriter);
+export function writer<W, T>(written: W[], value: T): Writer<W, T> {
+  return new Writer({ value, written });
 }
 
-export function tell<W>(msg: W): Writer<W, number> {
-  return writer({ value: 0, written: [msg] });
+export function tell<W>(msg: W): Writer<W, void> {
+  return new Writer({ value: void null, written: [msg] });
 }
