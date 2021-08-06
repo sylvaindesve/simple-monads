08cc5aeed34214766ee1638108a4409b554542a8
Sylvain Desvé <sylvain.desve@gmail.com>
2021-08-06 13:57:15 +0200

    Add fantasy-land references

--- src/Monad.ts
+++ src/Monad.ts
@@ -2,14 +2,17 @@ export interface Eq<T> {
   equals(other: T): boolean;
 }
 
+// https://github.com/fantasyland/fantasy-land#functor
 export interface Functor<T> {
   map<U>(fn: (t: T) => U): Functor<U>;
 }
 
+// https://github.com/fantasyland/fantasy-land#apply
 export interface Apply<T> {
   ap<U>(fn: Apply<(t: T) => U>): Apply<U>;
 }
 
+// https://github.com/fantasyland/fantasy-land#chain
 export interface Chain<T> {
   chain<U>(fn: (t: T) => Chain<U>): Chain<U>;
 }
